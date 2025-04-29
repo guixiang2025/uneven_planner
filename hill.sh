50bd4ec9f0596bdc6b87bb0e1b157c5e058f20d6
“guixiang2025” <fanguixiang2022m@gmail.com>
2025-04-29 20:54:19 +0800

    Feat: Add gitignore and commit essential server changes

--- hill.sh
+++ hill.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
-source ${CURRENT_DIR}/devel/setup.bash
+#source ${CURRENT_DIR}/devel/setup.bash
+# 计算工作空间根目录 (向上两级)
+CATKIN_WS_DIR="$( cd "${CURRENT_DIR}/../../" && pwd )"
+# Source 正确的 setup.bash
+source ${CATKIN_WS_DIR}/devel/setup.bash
 
 roslaunch plan_manager run_hill.launch & sleep 1;
 wait;
